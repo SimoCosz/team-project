32470e82754770343f85e02351732070bafb786e
AlfaProgrammer <alexloghin@outlook.it>
2022-04-30 11:16:14 +0200

    Creato layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Project</title>
+</head>
+<body>
+    
+    <header>
+        <h2>Questo è il nostro bruttissimo header </h2>
+    </header>
+
+    @yield('content')
+
+    <footer>
+        <h2>Questo è il nostro bruttissimo Footer</h2>
+    </footer>
+
+</body>
+</html>
