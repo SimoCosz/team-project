678f13ddb8019cc33c0a348dc9acff2fa1b5cfe9
coszach@hotmail.it <coszach@hotmail.it>
2022-04-30 10:20:21 +0200

    init table, seeder

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,6 +12,20 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        
+        foreach($comics as $c){
+            $comics = new Comic();
+
+            $comics->title = $c['title'];
+            $comics->description = $c['description'];
+            $comics->thumb = $c['thumb'];
+            $comics->price = $c['price'];
+            $comics->series = $c['series'];
+            $comics->sale_date = $c['sale_date'];
+            $comics->type = $c['type'];
+            
+            $comics->save();
+        }
     }
 }
